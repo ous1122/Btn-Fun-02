b2eec4efb34295e7eeb3816a126d22e1e5c52b6c
D7703_13 <d7703_13@D7703-13ui-iMac.local>
2018-03-12 15:50:00 +0900

    123

--- Btn Fun 02/ViewController.swift
+++ Btn Fun 02/ViewController.swift
@@ -13,7 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        view.backgroundColor = UIColor.black
+        view.backgroundColor = UIColor.yellow
+        
     }
 
     override func didReceiveMemoryWarning() {
